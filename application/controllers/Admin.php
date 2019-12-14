b4f55418f4525fabc269d2872f8b1a33eb17f97a
Angelica Kirana <aksd.70@hotmail.com>
2019-12-14 14:55:53 +0700

    Add files via upload

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,12 +4,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
  *
  */
 class Admin extends CI_Controller
-{
+{	
+		public function __construct(){
+			parent::__construct();
+			$this->load->model('Admin_model');
+		}
 
 
 public function index()
 {
-  // code...
+  $data['admin']=$this->Admin_model->get_all_admin();
+  $this->load->view('admin/login');
 }
 
 
