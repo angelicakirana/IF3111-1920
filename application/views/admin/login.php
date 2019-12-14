552c635ef65dc8e4c8171ea90f29c114ff61621f
Angelica Kirana <aksd.70@hotmail.com>
2019-12-14 22:50:52 +0700

    Add files via upload

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -3,7 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-body, html {
+html {
   height: 100%;
   font-family: Arial, Helvetica, sans-serif;
   background-image: url("assets/backlogin.png");
@@ -49,14 +49,41 @@ input[type=text]:focus, input[type=password]:focus {
 .btn:hover {
   opacity: 1;
 }
+.alert {
+  padding: 20px;
+  background-color: #f44336;
+  color: white;
+}
+
+.closebtn {
+  margin-left: 15px;
+  color: white;
+  font-weight: bold;
+  float: right;
+  font-size: 20px;
+  line-height: 10px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.closebtn:hover {
+  color: black;
+}
 </style>
 <title>Masuk Admin</title>
 </head>
 <body>
 
 <div class="bg-img">
-  <form action="<?php echo base_url('admin'); ?>" class="container" method="post">
-    <h1>Login</h1>
+  <form action="<?php echo base_url('auth/login'); ?>" class="container" method="post">
+    <h1>Masuk</h1>
+    <?php if($this->session->flashdata('login')): ?>
+    <div class="alert">
+      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+      <?php echo $this->session->flashdata('login'); ?>
+    </div>
+  <?php endif; ?>
+  <br>
     <label for="email"><b>Nama Pengguna</b></label>
     <input type="text" placeholder="Masukkan Nama Pengguna" name="username" required autocomplete="off">
     <label for="psw"><b>Kata Sandi</b></label>
